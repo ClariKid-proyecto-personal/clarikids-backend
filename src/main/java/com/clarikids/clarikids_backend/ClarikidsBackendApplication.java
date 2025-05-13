b5f6359c02eabc19dcdf58f528d7a7a98e194949
edmimi <mirisaor02@gmail.com>
2025-05-13 11:26:50 +0200

    Init Spring Boot backend project

---
+++ src/main/java/com/clarikids/clarikids_backend/ClarikidsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clarikids.clarikids_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClarikidsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClarikidsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clarikids/clarikids_backend/ClarikidsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarikids.clarikids_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClarikidsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
