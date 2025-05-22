96d41702a52519006ae52bbf3f53e29d999848e3
edmimi <mirisaor02@gmail.com>
2025-05-22 10:55:07 +0200

    DELETE- @crosOrigin

--- src/main/java/com/clarikids/clarikids_backend/controller/QuestionController.java
+++ src/main/java/com/clarikids/clarikids_backend/controller/QuestionController.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/questions")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 
  
 public class QuestionController {
